31096806efa6fae17cb02f6c05840656451e6710
Jinu <steve.luke1457@gmail.com>
2025-11-01 19:14:45 +0300

    Delete js directory

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const bubble = document.getElementById('control-bubble');
-const panel = document.getElementById('control-panel');
-
-bubble.addEventListener('click', () => {
-  panel.classList.toggle('hidden');
-});
--- js/theme.js
+++
@@ -1,4 +0,0 @@
-const themeBtn = document.getElementById('theme-toggle');
-themeBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-});
